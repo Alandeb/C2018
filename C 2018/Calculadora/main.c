2de68c9f1df857ef7cfd987a9fed3cd43819ec61
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-09-03 22:25:03 -0300

    Add files via upload

--- C 2018/Calculadora/main.c
+++ C 2018/Calculadora/main.c
@@ -3,90 +3,72 @@
 #include <conio.h>
 #define ESC 27
 
-int MENU();
-float ingresenumero(float);
-float suma();
-float resta();
-float producto();
-float dividir();
-int factorial(int);
-int MENUdos();
-char seguir;
-float numerouno,numerodos;
+int menu(float,float);
+float ingreseNumero(float);
+float suma(float , float);
+float resta(float , float);
+float producto(float , float);
+float dividir(float, float);
+long factorial(long);
+char menuDos(float,float);
+
 int main()
 {
-
-    int opcion,banderauno=0,banderados=0,opcioncalcular;
-    float ressuma,resresta,resproducto,resdividir,resfactorialA,resfactorialB;
-
+    int opcion,banderaUno=0,banderaDos=0;
+    char opcionCalcular;
+    float numeroUno,numeroDos,resSuma,resResta,resProducto,resDividir;
+    long resFactorialA,resFactorialB;
+    numeroUno=0;
+    numeroDos=0;
     do{
         system("cls");
-        opcion=MENU();
+        opcion=menu(numeroUno,numeroDos);
         switch(opcion){
             case 1:
-                numerouno=ingresenumero(numerouno);
-                if(seguir!=ESC){
-                    banderauno=1;
+                numeroUno=ingreseNumero(numeroUno);
+                    banderaUno=1;
                     break;
-                }else{
-                    seguir=0;
-                    break;
-                }
             case 2:
-                numerodos=ingresenumero(numerodos);
-                if(seguir!=ESC){
-                    banderados=1;
+                    numeroDos=ingreseNumero(numeroDos);
+                    banderaDos=1;
                     break;
-                }else{
-                    seguir=0;
-                    break;
-                }
             case 3:
-
-                if(banderauno==1&&banderados==1){
+                system("cls");
+                if(banderaUno==1&&banderaDos==1){
                         system("cls");
-                        opcioncalcular=MENUdos();
+                        opcionCalcular=menuDos(numeroUno,numeroDos);
                 }else{
                     system("cls");
-                    printf("bandera %d y %d",banderauno,banderados);
                     printf("Le falta algun numero ");
                     getch();
                     break;
                 }
-                switch(opcioncalcular){
-                            case 1:
-                                ressuma=suma();
+                switch(opcionCalcular){
+                            case 'a':
+                                resSuma=suma(numeroUno,numeroDos);
                                 break;
-                            case 2:
-                                resresta=resta();
+                            case 'b':
+                                resResta=resta(numeroUno,numeroDos);
                                 break;
-                            case 3:
-                                resproducto=producto();
+                            case 'c':
+                                resProducto=producto(numeroUno,numeroDos);
                                 break;
-                            case 4:
-                                if(numerodos!=0){
-                                    resdividir=dividir();
+                            case 'd':
+                                if(numeroDos!=0){
+                                    resDividir=dividir(numeroUno,numeroDos);
                                     break;
                                 }else{
-                                    system("cls");
-                                    printf("MATH ERROR");
-                                    printf("\nNo es posible dividir por cero volver a intentar ");
-                                    getch();
                                     break;
                                 }
-                            case 5:
-                                if(numerouno>=1){
-                                    resfactorialA=factorial(numerouno);
-                                    break;
-                                }else{
-                                    system("cls");
-                                    printf("No es posible hacer el factorial de cero o un numero negativo");
-                                    getch();
+                            case 'e':
+                                if(numeroUno>=1){
+                                    resFactorialA=factorial(numeroUno);
                                     break;
                                 }
-                            case 6:
-                                if(numerodos>=1){
-                                    resfactorialB=factorial(numerodos);
+                                break;
+                            case 'f':
+                                if(numeroDos>=1){
+                                    resFactorialB=factorial(numeroDos);
                                     break;
                                 }else{
                                     system("cls");
@@ -94,18 +76,18 @@ int main()
                                     getch();
                                     break;
                                 }
-                            case 7:
-                                ressuma=suma();
-                                resresta=resta();
-                                resproducto=producto();
-                                if(numerodos!=0){
-                                    resdividir=dividir();
+                            case 'g':
+                                resSuma=suma(numeroUno,numeroDos);
+                                resResta=resta(numeroUno,numeroDos);
+                                resProducto=producto(numeroUno,numeroDos);
+                                if(numeroDos!=0){
+                                    resDividir=dividir(numeroUno,numeroDos);
                                 }
-                                if(numerouno>=1){
-                                    resfactorialA=factorial(numerouno);
+                                if(numeroUno>=1){
+                                    resFactorialA=factorial(numeroUno);
                                 }
-                                if(numerodos>=1){
-                                    resfactorialB=factorial(numerodos);
+                                if(numeroDos>=1){
+                                    resFactorialB=factorial(numeroDos);
                                 }
                                 system("cls");
                                 printf("Operaciones hechas vaya a la opcion 4 para los resultados ");
@@ -114,13 +96,57 @@ int main()
                             default:
                                 break;
                         }
-                   break;
             case 4:
                 system("cls");
-                numerouno=0;
-                numerodos=0;
-                banderauno=0;
-                banderados=0;
+                switch(opcionCalcular){
+                    case 'a':
+                        printf("La suma es %.2f",resSuma);
+                        getch();
+                        break;
+                    case 'b':
+                        printf("La resta es %.2f",resResta);
+                        getch();
+                        break;
+                    case 'c':
+                        printf("El producto es %.2f",resProducto);
+                        getch();
+                        break;
+                    case 'd':
+                        if(numeroDos!=0){
+                            printf("La division es %.2f",resDividir);
+                            getch();
+                            break;
+                        }else{
+                            printf("MATH ERROR");
+                            printf("\nNo es posible dividir por cero volver a intentar ");
+                            getch();
+                            break;
+                         }
+                    case 'e':
+                         if(numeroUno>=1){
+                                printf("El numero factorial de %.0f es %ld",numeroUno,resFactorialA);
+                                break;
+                            }else{
+                                printf("No es posible hacer el factorial de cero o un numero negativo");
+                                getch();
+                                break;
+                            }
+                    case 'f':
+                        if(numeroDos>=1){
+                                printf("El numero factorial de %.0f es %ld",numeroDos,resFactorialB);
+                                break;
+                            }else{
+                                printf("No es posible hacer el factorial de cero o un numero negativo");
+                                getch();
+                                break;
+                            }
+                    default:
+                        break;
+                }
+                numeroUno=0;
+                numeroDos=0;
+                banderaUno=0;
+                banderaDos=0;
                 break;
             case 5:
                 system("cls");
@@ -133,26 +159,22 @@ int main()
 
     return 0;
 }
-float ingresenumero(float operando){
+float ingreseNumero(float operando){
           system("cls");
           printf("Coloque un numero o 'esc' para cancelar ");
-          seguir=getch();
-          if(seguir!=ESC){//operador imbecil hecho por si entro por equivocacion
-                scanf("%f",&operando);
-            }
-         return operando;
-
+          scanf("%f",&operando);
+          return operando;
 }
 
 
 
 
-int MENU(){
+int menu(float numeroUno,float numeroDos){
         int opcion;
         printf("MENU");
-        printf("\n\n1. Ingresar 1er operando(A=%.2f)",numerouno);
-        printf("\n2. Ingresar 2do operando(B=%.2f)",numerodos);
-        printf("\n3. Calcular todas las operaciones()si me da el tiempo separarlas");
+        printf("\n\n1. Ingresar 1er operando(A=%.2f)",numeroUno);
+        printf("\n2. Ingresar 2do operando(B=%.2f)",numeroDos);
+        printf("\n3. Calcular las operaciones");
         printf("\n4. Informar resultados");
         printf("\n5. Salir");
         printf("\n\n Seleccione opcion: ");
@@ -160,45 +182,44 @@ int MENU(){
         return opcion;
 
 }
-int MENUdos(){
-        int opciondos;
+char menuDos(float numeroUno,float numeroDos){
+        char opcion;
         printf("Calculos");
-        printf("\n\n1. Suma de %.2f+%.2f",numerouno,numerodos);
-        printf("\n2. Resta de %.2f-%.2f",numerouno,numerodos);
-        printf("\n3. Producto de %.2f*%.2f",numerouno,numerodos);
-        printf("\n4. division de %.2f/%.2f",numerouno,numerodos);
-        printf("\n5. Factorial de %.0f!",numerouno);
-        printf("\n6. Factorial de %.0f!",numerodos);
-        printf("\n7. Hacer todos los calculos");
-        printf("\n8. Cancelar");
+        printf("\n\nA) Suma de %.2f+%.2f",numeroUno,numeroDos);
+        printf("\nB) Resta de %.2f-%.2f",numeroUno,numeroDos);
+        printf("\nC) Producto de %.2f*%.2f",numeroUno,numeroDos);
+        printf("\nD) division de %.2f/%.2f",numeroUno,numeroDos);
+        printf("\nE) Factorial de %.0f!",numeroUno);
+        printf("\nF) Factorial de %.0f!",numeroDos);
+        printf("\nG) Hacer todos los calculos");
         printf("\n\n Seleccione opcion: ");
-        scanf("%d",&opciondos);
-        return opciondos;
+        scanf("%c",&opcion);
+        return opcion;
 }
-float suma(){
+float suma(float numeroUno, float numeroDos){
     float resultado;
-    resultado=numerouno+numerodos;
+    resultado=numeroUno+numeroDos;
     return resultado;
 }
-float resta(){
+float resta(float numeroUno, float numeroDos){
     float resultado;
-    resultado=numerouno-numerodos;
+    resultado=numeroUno-numeroDos;
     return resultado;
 }
-float producto(){
+float producto(float numeroUno, float numeroDos){
     float resultado;
-    resultado=numerouno*numerodos;
+    resultado=numeroUno*numeroDos;
     return resultado;
 }
-float dividir(){
+float dividir(float numeroUno, float numeroDos){
     float resultado;
-    resultado=numerouno/numerodos;
+    resultado=numeroUno/numeroDos;
     return resultado;
 }
-int factorial(int numero){
-      int b ,  fact = 1;
-      for (b = numero; b > 1; b--){
-            fact = fact * b;
+long factorial(long numero){
+      long numeFact ,fact = 1;
+      for (numeFact = numero; numeFact > 1; numeFact--){
+            fact *=  numeFact;
        }
        return fact;
 }
