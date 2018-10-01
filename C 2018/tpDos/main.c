4579528c01e1baf03a7e0239b3c8cbc4708789d9
Alandeb <40776573+Alandeb@users.noreply.github.com>
2018-10-01 12:43:19 -0300

    tp completo creo

--- C 2018/tpDos/main.c
+++ C 2018/tpDos/main.c
@@ -8,7 +8,8 @@
 int main()
 {
     Employee empleado[personal];
-    int opcion,alta,flag=0,auxId,valID=-1,posicionID=-1;
+    int opcion,alta,flag=0,auxId,valID=-1,posicionID=-1,opcionDos,order,i,accountantAverage=0,accountantMedia=0;
+    float promedio,salaryTotal=0;
     init(empleado,personal);
     do
     {
@@ -17,13 +18,14 @@ int main()
         {
             case 1:
                 alta=addEmployee(empleado,personal);
-                if(alta==0)
+                if(alta==0&&flag==0)
                     flag=1;
                 break;
             case 2:
                 if(flag==1){
+                    system("cls");
                     printf("\tMODIFICACIONES\n\n");
-                    valID=utn_getEntero(&auxId,"Ingrese ID del empleado que desea modificar: ","ERROR el id es numerico entre 1000 a 10000: ",1000,10000);
+                    valID=utn_getEntero(&auxId,"Ingrese ID del empleado que desea modificar: ","ERROR el id es numerico entre 1000 a 10000 o incorrecto: ",1000,10000);
                     if(valID==0)
                     {
                         posicionID=findEmployeeById(empleado,personal,auxId);
@@ -32,6 +34,10 @@ int main()
                             break;
                         }
                     }
+                }else{
+                    system("cls");
+                    printf("aun no ingreso ningun empleado\n");
+                    system("pause");
                 }
                 break;
             case 3:
@@ -48,9 +54,53 @@ int main()
                                 break;
                             }
                      }
+                }else{
+                    system("cls");
+                    printf("aun no ingreso ningun empleado\n");
+                    system("pause");
                 }
                 break;
             case 4:
+                if(flag==1){
+                    opcionDos=menuDos();
+                    switch(opcionDos){
+                        case 1:
+                            system("cls");
+                            utn_getEntero(&order,"INGRESE EL CRITERIO DE ORDENAMIENTO:  0(de menor a mayor) o 1 (de mayor a menor)  \n",
+                                          "ERROR INGRESE EL CRITERIO DE ORDENAMIENTO: 0(de menor a mayor) o 1 (de mayor a menor)  \n",0,1);
+                            sortEmployees(empleado,personal,order);
+                            for(i=0;i<personal;i++){
+                                if(empleado[i].isEmpty==0){
+                                    printEmployee(empleado,i);
+                                }
+                            }
+                            system("pause");
+                            break;
+                        case 2:
+                            system("cls");
+                            for(i=0;i<personal;i++){
+                                if(empleado[i].isEmpty==0){
+                                    salaryTotal=salaryTotal+empleado[i].salary;
+                                    accountantAverage++;
+                                }
+                            }
+                            promedio=salaryTotal/accountantAverage;
+                            for(i=0;i<personal;i++){
+                                if(empleado[i].salary>promedio&&empleado[i].isEmpty==0){
+                                    accountantMedia++;
+                                }
+                            }
+                            printf("Salario total: %.2f\tMedio de salarios: %.2f\tEmpleados que superan la media: %d\n",salaryTotal,promedio,accountantMedia);
+                            system("pause");
+                            break;
+                        default:
+                            break;
+                    }
+                }else{
+                    system("cls");
+                    printf("aun no ingreso ningun empleado \n");
+                    system("pause");
+                }
                 break;
             case 5:
                 system("cls");
--- C 2018/tpDos/utn_abm.h
+++ C 2018/tpDos/utn_abm.h
@@ -10,23 +10,26 @@ typedef struct
     int sector;
     int isEmpty;
 }Employee;
-void init(Employee perso[],int cantidad);
+void init(Employee* empleado,int cantidad);
 int addEmployee(Employee empleado[],int cantidad);
 void printEmployee(Employee* empleado,int i);
 int findEmployeeById(Employee* empleado, int cantidad,int id);
 void modificationEmployee(Employee* empleado,int posID);
 void removeEmployee(Employee* empleado,int posID);
+void sortEmployees(Employee empleado[], int cantidad ,int order);
 int validRamdonNumber(Employee empleado[],int cantidad);
 int getNumeroAleatorio(int desde , int hasta);
-int obtenerEspacioLibre(Employee perso[] , int cantidad);
+int obtenerEspacioLibre(Employee empleado[] , int cantidad);
 int esSoloLetras(char str[]);
 int getStringLetras(char* msg,char* msgErr,char* pBuffer);
 void getString(char msg[],char* pBuffer);
 int utn_getFloat(float *pFlotante, char* msg ,char* msgErr,int min);
+int esNumerico(char str[]);
 int getFloat(float* pBuffer);
 int utn_getEntero(  int* pEntero, char* msg,char* msgErr,int min, int max);
 int getInt(int* pBuffer);
 int utn_getCaracter(  char* pCaracter, char* msg,char* msgErr,char letraUno,char letraDos);
 int getCaracter(char* pBuffer);
 int menu();
+int menuDos();
 #endif // UTN_ABM_H_INCLUDE
